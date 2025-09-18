347ad7a5c73bb6feded69ec6b0b4f44c9e3af9d4
AndersonHJP <anderson.hericles68@gmail.com>
2025-09-17 21:33:52 -0300

    atualizando branch

---
+++ src/main/java/com/ifestapi/model/UserAccount.java
@@ -0,0 +1,46 @@
+package com.ifestapi.model;
+
+import com.ifestapi.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String document;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Boolean consentAccepted;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+
+}
